fb46362d778fc2de22a177367d942276bd55df75
Marc <marcmanuelmarquez4mail@gmail.com>
2025-09-28 15:18:30 +0800

    Trying out Kotlin

--- app/src/main/java/com/android/inventorytracking/MainActivity.kt
+++ app/src/main/java/com/android/inventorytracking/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.inventorytracking.ui.theme.InventoryTrackingTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +42,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(
+        color = Color.Black,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = modifier
+                .padding(16.dp)
+                .horizontalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = "Hello $name! where did you come from",
+                color = Color.White,
+                fontSize = 30.sp
+            )
+            Text(
+                text = "Hands Up Gabriela",
+                color = Color.White,
+                fontSize = 30.sp
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    name = "The Preview"
+)
 @Composable
 fun GreetingPreview() {
     InventoryTrackingTheme {
-        Greeting("Android")
+        Greeting(
+            "Crammm"
+            )
     }
 }
